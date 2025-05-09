b1e3d4056b3a41c6313fa5ac6f1f13247c73fa6b
Brayden <109638@swcsd.us>
2025-05-09 08:14:58 -0400

    Initial Commit

---
+++ potato/potatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  potatoApp.swift
+//  potato
+//
+//  Created by Brayden Bonnell-wright on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct potatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ potatoTests/potatoTests.swift
@@ -0,0 +1,17 @@
+//
+//  potatoTests.swift
+//  potatoTests
+//
+//  Created by Brayden Bonnell-wright on 5/9/25.
+//
+
+import Testing
+@testable import potato
+
+struct potatoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

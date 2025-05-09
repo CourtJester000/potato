fd40e9921de6559bd26a1c953ef4fd8672aa8645
Brayden <109638@swcsd.us>
2025-05-09 08:24:32 -0400

    made small change, test.

--- potato/ContentView.swift
+++ potato/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "circle")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Brayden - Mashed")
+            Text("BOW DOWN MERE MORTAL!!!!!!!")
         }
         .padding()
     }
